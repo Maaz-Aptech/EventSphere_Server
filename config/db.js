a88ad4c58378c8ace4cf7c684ea90f6059eb7140
Syed Muhammad Maaz Hassan <smmaazaptechmlr@gmail.com>
2025-09-18 14:01:35 +0500

    check4

--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
+let isConnected = false; // track connection
 
 const connectDB = async () => {
-try {
-const conn = await mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-});
-console.log(`MongoDB Connected: ${conn.connection.host}`);
-} catch (error) {
-console.error(`Error: ${error.message}`);
-process.exit(1);
-}
+  if (isConnected) return;
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = conn.connections[0].readyState === 1;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    throw err;
+  }
 };
 
-
-module.exports = connectDB;
+export default connectDB;
